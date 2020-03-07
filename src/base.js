2ba7c8b1582949ef01a0aa8a9b341a021b3fc312
Jassem Gaaloul <jassemgaaloul123@gmail.com>
2020-03-07 03:38:23 +0100

    first commit

---
+++ src/base.js
@@ -0,0 +1,11 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyDCNVb4VTdo-VQhHhLLhwIOc_-vCHfDcx8",
+  authDomain: "catch-fish-27af7.firebaseapp.com",
+  databaseURL: "https://catch-fish-27af7.firebaseio.com",
+});
+
+
+
+export default base;
